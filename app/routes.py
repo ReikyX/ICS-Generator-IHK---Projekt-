1d010ef5b0d3702f9a48bbbe0fc8b2aae73c53fc
ReikyX <188848574+ReikyX@users.noreply.github.com>
2026-03-31 10:14:58 +0200

    add: smart.html, base.html(navbar), style.css(navbar, body, main and footer)

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/home')
 def index_page():
     return render_template('index.html')
+
+@app.route('/smart_text')
+def smart_page():
+    return render_template('smart.html')
